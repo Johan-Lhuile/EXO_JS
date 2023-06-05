add641e151a183e80f4446430fed9c87fbefa787
johan-lhuile <johanlhuile85@gmail.com>
2023-06-05 23:09:40 +0200

    suite du jeu

--- Le_juste_prix/script.js
+++ Le_juste_prix/script.js
@@ -3,57 +3,100 @@ const price = parseInt(Math.floor(Math.random() * 100));
 console.log(price);
 
 const message = document.createElement("h3");
-
+const decomptenb = document.querySelector("#decompte");
 const essai = document.querySelector("#essaiInput");
-
+const reload = document.querySelector("#reload");
+const submit = document.querySelector("#valider");
+let i = 7;
 function createNewH3(text) {
         
     message.textContent = text;
     document.querySelector("#messageh3").appendChild(message);
 }
 
-function decompte(text) {
-    message.textContent = i;
-        document.querySelector("#messageh3").appendChild(message);
+function decompte(i) {
+    decomptenb.textContent = i;
+        document.querySelector("#decompte").appendChild(message);
     }
 
+function gagner() {
 
-function action(input) {
 
-    for (let i = 0; i <= 7; i++) {
+    if (parseInt(essai.value) === price && 0 < i < 7) {
+
+    }
+}
+
+
+
+function perdu() { 
+    
+    if ( i == 0) {}
+
+}
+
+
+function propose(input) {
+
     
     
-    if (parseInt(essai.value) < price && i < 7) 
+    if (parseInt(essai.value) < price && 0 < i < 7) 
         
     {
         console.log("grand");
+        console.log(i);
+        createNewH3("le juste prix est plus grand");
 
-        createNewH3("le juste prix est plus grand")
     }
-    else if (parseInt(essai.value) > price && i < 7) {
+    else if (parseInt(essai.value) > price && 0 < i < 7) {
         console.log("petit");
-        
+        console.log(i);
         createNewH3("le juste prix est plus petit");
-
-    } else if (parseInt(essai.value) === price && i < 7) {
+        
+    } else if (parseInt(essai.value) === price && 0 < i < 7) {
         console.log("gagner");
-        
+        console.log(i);
         createNewH3("GAGNER!!!");
-    } else {
-        console.log("perdu");
         
+    } else if (i === 0) {
+
+        console.log("perdu");
+        console.log(i);
         createNewH3("PERDU, Allez recommencez");
     }
-console.log(input.value);
-}}
+// console.log(input.value);
+decompte("il te reste " + i + " essaie(s)")
+}
 
 essai.addEventListener("keydown", function (event) {
-  if (event.key === "Enter") {
+if (event.key === "Enter" && 0 < i < 7) {
     console.log(event);
-    action(this);
-    // action(event.target);
-  }
-});
+    
+    propose(this);
+    i--
+    
+} else if (i === 0) {
+    console.log("perdu");
+    console.log(i);
+    createNewH3("PERDU, Allez recommencez");
+}  
+})
+
+submit.addEventListener("clic", function (propose) {
+    
+        
+        propose(this);
+        
+    }
+    );
+    
+
+reload.addEventListener("clic", function () {
+    
+        window.location.reload()
+    }
+    );
+    
 
 
     
