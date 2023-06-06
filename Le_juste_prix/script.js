18451beaf7be5f88f92cdda043ca1db7826ecb08
johan-lhuile <johanlhuile85@gmail.com>
2023-06-06 19:21:57 +0200

    ajout du CSS

--- Le_juste_prix/script.js
+++ Le_juste_prix/script.js
@@ -1,13 +1,13 @@
 const price = parseInt(Math.floor(Math.random() * 100));
 
-console.log(price);
+// console.log(price);
 
 const message = document.createElement("h3");
-const decomptenb = document.querySelector("#decompte");
+const decomptenb = document.createElement("h3");
 const essai = document.querySelector("#essaiInput");
 const reload = document.querySelector("#reload");
 const submit = document.querySelector("#valider");
-let i = 7;
+let i = 10;
 
 const recompense = ["un apple wish", "un sushi en plastique", "une boite à meeeeuuuhhhh"]
 
@@ -22,14 +22,14 @@ function createNewH3(text) {
 
 function decompte(i) {
     decomptenb.textContent = i;
-        document.querySelector("#decompte").appendChild(message);
+        document.querySelector("#decompte").appendChild(decomptenb);
     }
 
 function gagner() {
 
     if (parseInt(essai.value) === price && 0 < i ) {
-        console.log("gagner");
-        console.log(i);
+        // console.log("gagner");
+        // console.log(i);
         createNewH3("FELICITATION!!!! Vous avez gagné un(e) " + recompense[randomrecompense]);
         essai.disabled = true; 
         submit.disabled = true; 
@@ -38,12 +38,10 @@ function gagner() {
     }
 }
 
-
-
 function perdu() { 
     
-    console.log("perdu");
-    console.log(i);
+    // console.log("perdu");
+    // console.log(i);
     createNewH3("PERDU, Allez recommencez");
     essai.disabled = true; 
     submit.disabled = true;
@@ -58,21 +56,23 @@ function propose(input) {
         return;
     }
     
-    if (parseInt(essai.value) < price) 
-        
-    {
-        console.log("grand");
-        console.log(i);
+    if (parseInt(essai.value) < price) {
+
+        // console.log("grand");
+        // console.log(i);
         createNewH3("le juste prix est plus grand");
         essai.value = "";
 
     }
     else if (parseInt(essai.value) > price) {
-        console.log("petit");
-        console.log(i);
+
+        // console.log("petit");
+        // console.log(i);
         createNewH3("le juste prix est plus petit");
         essai.value = "";
+
     } else {
+
         gagner()
         
 }   
@@ -81,17 +81,16 @@ decompte("il te reste " + i + " essaie(s)")
 }
 
 essai.addEventListener("keydown", function (event) {
+
 if (event.key === "Enter" && i > 0) {
-    console.log(event);
+    // console.log(event);
     i--
     propose(this);
-    // essai.value = "";
-} 
-
+    } 
 })
 
-
 submit.addEventListener("click", function () {
+
     if ( i > 0) {
         i--
         propose(this);
